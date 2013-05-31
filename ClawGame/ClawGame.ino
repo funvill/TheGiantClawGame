d8dc92112ffd3a21e7b5002f95b7ce587c6a25e1
funvill <funvill@funvill.com>
2013-05-31 00:14:47 -0600

    Almost final update
    This version will be very close to the version that I use at Vancouver Maker Faire 2013

--- ClawGame/ClawGame.ino
+++ ClawGame/ClawGame.ino
@@ -1,4 +1,3 @@
-
 // Pins 
 // ------------------------------------------------------------------------------------
 
@@ -6,30 +5,27 @@ const int PIN_BUTTON_UP         = A2 ;
 const int PIN_BUTTON_RIGHT      = A1 ; 
 const int PIN_BUTTON_DOWN       = A0 ; 
 const int PIN_BUTTON_LEFT       = A3 ; 
-const int PIN_BUTTON_FIRE       = A4 ;
+const int PIN_BUTTON_A          = A4 ;
+const int PIN_BUTTON_B          = A5 ;
 
-const int PIN_LIMIT_CLAW_MAX    =  2 ; 
-const int PIN_LIMIT_CLAW_MIN    =  3 ; 
+const int PIN_LIMIT_CLAW_MAX    =  5 ; 
+const int PIN_LIMIT_CLAW_MIN    =  4 ; 
 
-const int PIN_LIMIT_NORTH       = 4 ; 
-const int PIN_LIMIT_EAST        = 5 ; 
-const int PIN_LIMIT_SOUTH       = 6 ; 
-const int PIN_LIMIT_WEST        = 7 ; 
-
-const int PIN_MOTOR_CLAW_IN1    = 8;
-const int PIN_MOTOR_CLAW_IN2    = 9;
-
-const int PIN_MOTOR_ONE_IN1    = 10;
-const int PIN_MOTOR_ONE_IN2    = 11;
-
-const int PIN_MOTOR_TWO_IN1    = 12;
-const int PIN_MOTOR_TWO_IN2    = 13;
-
+const int PIN_MOTOR_CLAW_TEETH_IN1  =  6;
+const int PIN_MOTOR_CLAW_TEETH_IN2  =  7;
+const int PIN_MOTOR_CLAW_CABLE_IN1  =  8;
+const int PIN_MOTOR_CLAW_CABLE_IN2  =  9;
+const int PIN_MOTOR_TWO_IN1         = 10;
+const int PIN_MOTOR_TWO_IN2         = 11;
+const int PIN_MOTOR_ONE_IN1         = 12;
+const int PIN_MOTOR_ONE_IN2         = 13;
 
 
 // Constants 
 // ------------------------------------------------------------------------------------
-const int MIN_BUTTON_VALUE   = 500; 
+const int  MIN_BUTTON_VALUE     = 1000; 
+const long TIME_BUTTON_DEBOUNCE = 100;
+const int  MAX_CABLE_LENGTH     = 10; 
 
 // ------------------------------------------------------------------------------------
 
@@ -72,39 +68,63 @@ class CMotor {
 
 // ------------------------------------------------------------------------------------
 
-class CAButton
+
+
+class CButton
 {
   private:
     int m_pin; 
+    boolean m_value ; 
+    boolean m_lastButtonState ; 
+    long m_lastDebounceTime ; 
     
   public:
-    void Init( int pin, int type=INPUT ) {
-      this->m_pin   = pin; 
+    void Init( int pin, int type=INPUT ) {      
+      this->m_lastDebounceTime  = 0 ; 
+      this->m_lastButtonState   = false ;  
+      this->m_value             = false ; 
+      
+      this->m_pin = pin; 
       pinMode(this->m_pin, INPUT);
     }
+    int GetPin() {
+      return this->m_pin ; 
+    }
+    int ReadValue() {
+      return digitalRead( this->GetPin() ); 
+    }
     
-    bool GetValue() {
-      return ( analogRead( this->m_pin ) > MIN_BUTTON_VALUE ) ;
+    boolean GetValue() {
+      int reading   = this->ReadValue() ; 
+      long now      = (long)millis();    // get current time
+    
+      if( reading != this->m_lastButtonState ) {
+        this->m_lastDebounceTime = now ; 
+        this->m_lastButtonState  = reading ; 
+      }
+      
+      // debounce the button (Check if a stable, changed state has occured)
+      if (now - this->m_lastDebounceTime > TIME_BUTTON_DEBOUNCE  ) {
+        this->m_value = reading ;         
+      }
+      
+      /*
+      Serial.print( "P[" );
+      Serial.print( this->GetPin() );
+      Serial.print( "] reading [" );
+      Serial.print( reading ); 
+      Serial.print( "] value [" );
+      Serial.print( m_value );      
+      Serial.print( "] lastState [" );
+      Serial.print( m_lastButtonState );  
+      Serial.print( "] Delta [" );
+      Serial.print( now - this->m_lastDebounceTime );  
+      Serial.println( "]" );
+      */
+      
+      return this->m_value; 
     }    
-};
-
-class CDButton
-{
-  private:
-    int m_pin; 
-    
-  public:
-    void Init( int pin, int type=INPUT ) {
-      this->m_pin   = pin; 
-      pinMode(this->m_pin, INPUT);
-    }
-    
-    bool GetValue() {
-      return digitalRead( this->m_pin ) ;
-    }
-    
-};
-
+}; 
 
 
 // ------------------------------------------------------------------------------------
@@ -112,95 +132,145 @@ class CDButton
 class CClaw
 {
   private:
-    CAButton m_buttonMoveUp;
-    CAButton m_buttonMoveRight;
-    CAButton m_buttonMoveDown;
-    CAButton m_buttonMoveLeft;
-    CAButton m_buttonMoveFire;
+  
+    // Joystick. 
+    CButton m_buttonMoveUp;
+    CButton m_buttonMoveRight;
+    CButton m_buttonMoveDown;
+    CButton m_buttonMoveLeft;
     
-    CDButton m_buttonLimitClawMax;
-    CDButton m_buttonLimitClawMin;
-    CDButton m_buttonLimitNorth;
-    CDButton m_buttonLimitEast;
-    CDButton m_buttonLimitSouth;
-    CDButton m_buttonLimitWest;
+    // Buttons 
+    CButton m_buttonA;
+    CButton m_buttonB;
     
-    CMotor m_motorCable; 
-    CMotor m_motorClaw; 
+    // Limit switches 
+    CButton m_buttonLimitClawMax;
+    CButton m_buttonLimitClawMin;
+    
+    // Motors 
+    CMotor m_motorClawTeeth; 
+    CMotor m_motorClawCable; 
     CMotor m_motorOne; 
     CMotor m_motorTwo; 
 
+    // Claw movment. 
     void ActionMoveUp(); 
     void ActionMoveDown(); 
     void ActionMoveRight(); 
     void ActionMoveLeft(); 
-    void ActionFire(); 
+    
+    // Claw Action 
+    void ActionClawOpen();
+    void ActionClawClose();
+    void ActionClawRise();
+    void ActionClawDrop();
+    
+    boolean m_bClawCableState ; 
+    boolean m_bClawTeethState;
     
   public:
     void Init( ); 
-    void Loop();    
+    void Loop();
+    
+    void DelayWithUpdate( int time ) ; 
 };
 
 void CClaw::Init( ) {
-  
+     
   // Joystick 
   this->m_buttonMoveUp.Init    (PIN_BUTTON_UP    );
   this->m_buttonMoveRight.Init (PIN_BUTTON_RIGHT );
   this->m_buttonMoveDown.Init  (PIN_BUTTON_DOWN  );
   this->m_buttonMoveLeft.Init  (PIN_BUTTON_LEFT  );
-  this->m_buttonMoveFire.Init  (PIN_BUTTON_FIRE  );
   
-  // Cube limit switches 
-  this->m_buttonLimitNorth.Init (PIN_LIMIT_NORTH );
-  this->m_buttonLimitEast.Init  (PIN_LIMIT_EAST );
-  this->m_buttonLimitSouth.Init (PIN_LIMIT_SOUTH );
-  this->m_buttonLimitWest.Init  (PIN_LIMIT_WEST );
-  
-  // Claw 
+  // Buttons 
+  this->m_buttonA.Init         (PIN_BUTTON_A     );
+  this->m_buttonB.Init         (PIN_BUTTON_B     );
+
+  // Claw limit switches. 
   this->m_buttonLimitClawMax.Init( PIN_LIMIT_CLAW_MAX );
   this->m_buttonLimitClawMin.Init( PIN_LIMIT_CLAW_MIN );
   
   // Motors 
-  this->m_motorClaw.Init  ( PIN_MOTOR_CLAW_IN1, PIN_MOTOR_CLAW_IN2 ); 
-  this->m_motorCable.Init ( PIN_MOTOR_CLAW_IN1, PIN_MOTOR_CLAW_IN2 ); 
-  this->m_motorOne.Init   ( PIN_MOTOR_ONE_IN1,  PIN_MOTOR_ONE_IN2 ); 
-  this->m_motorTwo.Init   ( PIN_MOTOR_TWO_IN1,  PIN_MOTOR_TWO_IN2 ); 
+  this->m_motorClawCable.Init ( PIN_MOTOR_CLAW_CABLE_IN1,  PIN_MOTOR_CLAW_CABLE_IN2 ); 
+  this->m_motorClawTeeth.Init ( PIN_MOTOR_CLAW_TEETH_IN1,  PIN_MOTOR_CLAW_TEETH_IN2 ); 
+  this->m_motorOne.Init       ( PIN_MOTOR_ONE_IN1,         PIN_MOTOR_ONE_IN2 ); 
+  this->m_motorTwo.Init       ( PIN_MOTOR_TWO_IN1,         PIN_MOTOR_TWO_IN2 ); 
   
+  this->m_bClawCableState = false; 
+  this->m_bClawTeethState = false;
 }
 
+void CClaw::DelayWithUpdate( int time ) {
+  while(time > 0 ) {
+    delay(2);
+    time -= 2 ;
+    
+    // Joystick 
+    this->m_buttonMoveUp.GetValue();
+    this->m_buttonMoveRight.GetValue();
+    this->m_buttonMoveDown.GetValue();
+    this->m_buttonMoveLeft.GetValue();
+    
+    // Buttons 
+    this->m_buttonA.GetValue();
+    this->m_buttonB.GetValue();
+  
+    // Claw limit switches. 
+    this->m_buttonLimitClawMax.GetValue();
+    this->m_buttonLimitClawMin.GetValue(); 
+  }
+}
 
 void CClaw::Loop() {
-
-    // Start the fire 
-    // Once the player fires the craw the system is automatic afterwards. 
-    if( this->m_buttonMoveFire.GetValue() ) {
-      this->ActionFire();
-      return; 
-    }
+   
+    // Cable 
+    if( this->m_buttonA.GetValue() ) {
+        Serial.println( "m_buttonA" );
+        if( this->m_bClawCableState ) {      
+            this->ActionClawOpen(); 
+            this->m_bClawCableState = !this->m_bClawCableState ; 
+        } else {      
+            this->ActionClawClose();       
+            this->m_bClawCableState = !this->m_bClawCableState ; 
+        }     
+            
+        DelayWithUpdate(1000); 
+    }  
     
-    // Limit switches 
-    if( this->m_buttonLimitNorth.GetValue() ) {
-      Serial.println( "Limit North " ); 
-    } else if( this->m_buttonLimitSouth.GetValue() ) {
-      Serial.println( "Limit South " ); 
-    } else if( this->m_buttonLimitEast.GetValue() ) {
-      Serial.println( "Limit East " ); 
-    } else if( this->m_buttonLimitWest.GetValue() ) {
-      Serial.println( "Limit West " ); 
+    // Teeth 
+    if( this->m_buttonB.GetValue() ) {
+        Serial.println( "m_buttonB" );
+        if( this->m_bClawTeethState ) {      
+            this->ActionClawDrop();
+            this->m_bClawTeethState = !this->m_bClawTeethState ;
+        } else {      
+            this->ActionClawRise();
+            this->m_bClawTeethState = !this->m_bClawTeethState ;
+        }        
+        DelayWithUpdate(1000); 
+    }  
+      
+    if( this->m_buttonLimitClawMax.GetValue() ) {
+        Serial.println( "m_buttonLimitClawMax " );
     }
+    if( this->m_buttonLimitClawMin.GetValue() ) {
+        Serial.println( "m_buttonLimitClawMin " );
+    }  
+    
 
     // Move the motors
-    if( this->m_buttonMoveUp.GetValue() && ! this->m_buttonLimitNorth.GetValue() ) {
-      this->ActionMoveUp();       
-    } else if( this->m_buttonMoveDown.GetValue() && ! this->m_buttonLimitSouth.GetValue() ) {
-      this->ActionMoveDown();       
-    } else if( this->m_buttonMoveRight.GetValue() && ! this->m_buttonLimitEast.GetValue() ) {
-      this->ActionMoveRight();       
-    } else if( this->m_buttonMoveLeft.GetValue() && ! this->m_buttonLimitWest.GetValue() ) {
-      this->ActionMoveLeft();       
+    if( this->m_buttonMoveUp.GetValue() ) {
+        this->ActionMoveUp();       
+    } else if( this->m_buttonMoveDown.GetValue()  ) {
+        this->ActionMoveDown();       
+    } else if( this->m_buttonMoveRight.GetValue() ) {
+        this->ActionMoveRight();       
+    } else if( this->m_buttonMoveLeft.GetValue() ) {
+        this->ActionMoveLeft();       
     } else {
-      this->m_motorOne.Stop();
-      this->m_motorTwo.Stop();
+        this->m_motorOne.Stop();
+        this->m_motorTwo.Stop();
     }
 }
 
@@ -214,96 +284,85 @@ void CClaw::Loop() {
  */
 
 void CClaw::ActionMoveUp() {
-  // Reset the up button values 
-  Serial.println( "Action Up " );   
-  
-  this->m_motorOne.Forward();  
-  this->m_motorTwo.Reverse();
+    // Reset the up button values 
+    Serial.println( "Action Up " );   
+    this->m_motorOne.Forward();  
+    this->m_motorTwo.Reverse();
 }
 void CClaw::ActionMoveDown() {
-  // Reset the up button values 
-  Serial.println( "Action Down " );   
-  
-  this->m_motorOne.Reverse();
-  this->m_motorTwo.Forward();
+    // Reset the up button values 
+    Serial.println( "Action Down " );  
+    this->m_motorOne.Reverse();
+    this->m_motorTwo.Forward();
 }
 void CClaw::ActionMoveRight() {
-  // Reset the up button values 
-  Serial.println( "Action Right " );   
-  
-  this->m_motorOne.Forward(); 
-  this->m_motorTwo.Forward();   
+    // Reset the up button values 
+    Serial.println( "Action Right " ); 
+    this->m_motorOne.Forward(); 
+    this->m_motorTwo.Forward();   
 }
 void CClaw::ActionMoveLeft() {
-  // Reset the up button values 
-  Serial.println( "Action Left " );   
-  
-  this->m_motorOne.Reverse();
-  this->m_motorTwo.Reverse();
-}
-void CClaw::ActionFire() {
-  // Reset the up button values 
-  Serial.println( "Action Fire " );   
-  
-  
-  // Drop the claw in to the 
-  Serial.println( "Drop the claw" ); 
-  for( int i = 0 ; i < 10 ; i++ ) {
-    this->m_motorCable.Forward();
-  }
-
-  // Close the claw. until the limit switch has been research. 
-  Serial.println( "Close the claw." ); 
-  while( ! this->m_buttonLimitClawMin.GetValue() ) { 
-    this->m_motorClaw.Forward();
-  }
-  
-  // Rise the claw   
-  Serial.println( "Rise the claw." );   
-  for( int i = 0 ; i < 10 ; i++ ) {
-    this->m_motorCable.Reverse();
-  }
-  
-  // Move the claw back to the starting pos. 
-  Serial.println( "Move the claw to the right." );  
-  while( this->m_buttonLimitEast.GetValue() ) {
-    this->ActionMoveRight();  
-  }
-  Serial.println( "Move the claw to the south" );  
-  while( this->m_buttonLimitSouth.GetValue() ) {
-    this->ActionMoveDown();
-  }
-
-  // Drop the claw
-  Serial.println( "Open the claw" );  
-  while( ! this->m_buttonLimitClawMax.GetValue() ) { 
-    this->m_motorClaw.Reverse();
-  }
-  
-  Serial.println( "All done." );  
-  this->m_motorOne.Stop();
-  this->m_motorTwo.Stop();
-  
+    // Reset the up button values 
+    Serial.println( "Action Left " ); 
+    this->m_motorOne.Reverse();
+    this->m_motorTwo.Reverse();
 }
 
 
-CClaw gClaw; 
+
+void CClaw::ActionClawOpen() {
+    while( ! this->m_buttonLimitClawMax.GetValue() ) {
+        this->m_motorClawTeeth.Forward();
+        Serial.println( "ActionClawOpen" );   
+    }
+    this->m_motorClawTeeth.Stop();
+    Serial.println( "ActionClawOpen = done" );
+}
+void CClaw::ActionClawClose(){
+    while( ! this->m_buttonLimitClawMin.GetValue() ) {
+        this->m_motorClawTeeth.Reverse();
+        Serial.println( "ActionClawClose" );
+    }
+    this->m_motorClawTeeth.Stop();
+    Serial.println( "ActionClawClose = done" );
+}
+
+void CClaw::ActionClawRise() {  
+    for( int i = 0 ; i < MAX_CABLE_LENGTH ; i++ ) {
+        this->m_motorClawCable.Forward();
+        Serial.println( "ActionClawRise" );
+        DelayWithUpdate(500); 
+    }  
+    this->m_motorClawCable.Stop();
+    Serial.println( "ActionClawRise = done" );
+}
+
+void CClaw::ActionClawDrop() {
+    for( int i = 0 ; i < MAX_CABLE_LENGTH ; i++ ) {
+        this->m_motorClawCable.Reverse();
+        Serial.println( "ActionClawDrop" );
+        DelayWithUpdate(500); 
+    }
+    this->m_motorClawCable.Stop();
+    Serial.println( "ActionClawDrop = done" );
+}
+
+
+
 // ------------------------------------------------------------------------------------
-
-
-
+CClaw gClaw; 
 void setup() {
-  // initialize serial communications at 9600 bps:
-  Serial.begin(9600);   
-  gClaw.Init( );   
+    // initialize serial communications at 9600 bps:
+    Serial.begin(9600);   
+    gClaw.Init( );   
 }
 
-void loop() {
- 
-  gClaw.Loop(); 
-
-  // wait 2 milliseconds before the next loop
-  // for the analog-to-digital converter to settle
-  // after the last reading:
-  delay(2);                     
+void loop() { 
+    gClaw.Loop(); 
+    
+    // wait 2 milliseconds before the next loop
+    // for the analog-to-digital converter to settle
+    // after the last reading:
+    gClaw.DelayWithUpdate(2);                     
 }
+
